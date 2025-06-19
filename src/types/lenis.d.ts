34da652f112eb772b7d5c6ce7d49499e04a424db
zabirtech <sabirquazi@gmail.com>
2025-06-19 23:23:02 +0200

    feat: add type definitions for LenisScrollOptions and improve event callback types

--- src/types/lenis.d.ts
+++ src/types/lenis.d.ts
@@ -8,11 +8,21 @@ declare global {
 
 // Extend the Window interface to include Lenis
 declare module "lenis" {
+  interface LenisScrollOptions {
+    offset?: number;
+    duration?: number;
+    easing?: (t: number) => number;
+    immediate?: boolean;
+    lock?: boolean;
+    force?: boolean;
+    onComplete?: () => void;
+  }
+
   interface Lenis {
     scroll: number;
-    scrollTo: (target: string | number | HTMLElement, options?: any) => void;
-    on: (event: string, callback: Function) => void;
-    off: (event: string, callback: Function) => void;
+    scrollTo: (target: string | number | HTMLElement, options?: LenisScrollOptions) => void;
+    on: (event: string, callback: (args?: unknown) => void) => void;
+    off: (event: string, callback: (args?: unknown) => void) => void;
   }
 }
 
